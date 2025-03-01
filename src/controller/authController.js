5cc620cfecd3ffda8784acf489b1ed814bafe70c
Juanco <juanffo874@gmail.com>
2025-02-28 23:49:48 -0600

    Agrega la función de confirmar la cuenta

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -22,3 +22,23 @@ export const createdUser = async (req, res) => {
         return res.status(500).json({message: error.message})
     }
 }
+
+export const confirmAccount = async (req, res) => {
+    const { token } = req.params
+    const user = await User.findOne({token})
+
+    if(!user){
+        const error = new Error("El token no es valído")
+        return res.status(400).json({message: error.message})
+    }
+        user.token = null;
+        user.confirm = true;
+        await user.save()
+
+        res.json({msg: "Cuenta confirmada correctamente"})
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createdUser } from "../controller/authController.js";
+import { createdUser, confirmAccount } from "../controller/authController.js";
 
 const router = Router();
 
 router.post("/register", createdUser)
-router.get("confirm-account")
+router.get("confirm-account", confirmAccount)
 router.post("/login")
 router.post("/recovery-password")
 router.route("/recovery-password")
