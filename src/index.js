f59dc13b95453be7301485db40a5c9ebabe2869a
Juanco <juanffo874@gmail.com>
2025-02-28 22:11:14 -0600

    Establece conexion con la base de datos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js'
+import db from './config/db.js'
 
+// Initialize express app and define port number
 const app = express();
 
 const port = process.env.PORT || 4000;
@@ -13,3 +15,11 @@ app.use('/api/auth', authRoutes)
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
+
+try {
+  await db.authenticate();
+  db.sync();
+  console.log("Conexión correcta con la base de datos");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
