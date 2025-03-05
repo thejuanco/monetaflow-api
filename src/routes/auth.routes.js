f39adafe81d62c4e01985612330e08de0ffe94b6
Juanco <juanffo874@gmail.com>
2025-03-04 20:49:45 -0600

    Corrige algun error en el codigo

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,9 @@ import { createdUser, confirmAccount } from "../controller/authController.js";
 
 const router = Router();
 
+//Vistas publicas
 router.post("/register", createdUser)
-router.get("confirm-account", confirmAccount)
+router.get("/confirm-account/:token", confirmAccount)
 router.post("/login")
 router.post("/recovery-password")
 router.route("/recovery-password")
