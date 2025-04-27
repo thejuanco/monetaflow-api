8e93c461b613f303793e19bc6281c3a11bc377a4
Juanco <juanffo874@gmail.com>
2025-04-26 22:33:03 -0600

    Termina de crear el cuerpo de la función para crear la categoria

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -1,14 +1,35 @@
-import {Categories} from "../models/index.js"
+import { Categories, User } from "../models/index.js";
 
 export const addCategories = async (req, res) => {
-    console.log(req.body)
-}
+  try {
+    const { name, type } = req.body;
+    const userId = req.user.id;
+
+    if(!name || !type) {
+      return res.status(400).json({ message: "Faltan datos" });
+    }
+
+    const nameExists = await Categories.findOne({ where: { name }})
+    if (nameExists) {
+      return res.status(400).json({ message: "La categoria ya existe" });
+    }
+
+    const newCategory = await Categories.create({
+      name,
+      type,
+      userId,
+    });
+    return res.status(200).json(newCategory);
+  } catch (error) {
+    return res.status(500).json({ message: "Error al crear la categoria" });
+  }
+};
 
 export const getCategories = async (req, res) => {
-    try {
-        const categories = await Categories.findAll()
-        return res.status(200).json(categories)
-    } catch (error) {
-        return res.status(500).json({ message: "Error al obtener las categorias" })
-    }
-}
+  try {
+    const categories = await Categories.findAll();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(500).json({ message: "Error al obtener las categorias" });
+  }
+};
